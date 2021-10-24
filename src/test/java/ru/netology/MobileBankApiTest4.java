5cf8e658226628f74492fb09339972bbbd558137
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-10-24 23:35:17 +0300

    test added

---
+++ src/test/java/ru/netology/MobileBankApiTest4.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
+public class MobileBankApiTest4 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
+    }
+}
+
